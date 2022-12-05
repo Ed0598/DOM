838879e698f127227502f69040a2bba9d46891f4
Edouard de Romrée <deromree.edouard@gmail.com>
2022-12-05 15:22:34 +0100

    3.2

---
+++ 3.2.Events/script.js
@@ -0,0 +1,25 @@
+const _initTime = Date.now()
+
+const getElapsedTime = () => {
+  return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
+}
+
+const clickOnSquare = (e) => {
+  section=document.getElementsByTagName('section')[0];
+  let div=document.createElement('div')
+  div.classList.add("displayedsquare",e.target.classList[1]);
+  section.appendChild(div);
+  ul=document.getElementsByTagName('ul')[0];
+  let li= document.createElement('li')
+  li.textContent= (getElapsedTime(),e.target.classList[1])
+  ul.appendChild(li)
+  console.log(e.target.classList[1])
+  console.log(getElapsedTime())
+
+}
+
+const actionSquares = document.querySelectorAll('.actionsquare')
+for (let actionSquare of actionSquares) {
+  actionSquare.addEventListener('click', clickOnSquare)
+}
+
